a462a8f7b995101bcb4ccf6a471a866d0c2aa668
alexaiken <aiken@cs.stanford.edu>
2019-06-14 23:21:15 -0700

    updated index launch example

--- Examples/Tasks/indexlaunch/indexlaunch.cc
+++ Examples/Tasks/indexlaunch/indexlaunch.cc
@@ -8,39 +8,74 @@ using namespace Legion;
 // A global enum is a convenient way to assign task ids.
 enum TaskID {
   TOP_LEVEL_TASK_ID,
-  INDEX_PRODUCER_ID,
-  INDEX_CONSUMER_ID,
+  PRODUCER_ID,
+  CONSUMER_ID
 };
 
-class PCTask: public IndexLauncher {
+//
+// The recommended way to use index launches is to build on the base IndexLauncher class to define
+// launcher objects that encapsulate all of the information specific to launching index tasks of 
+// a particular kind.  Here we define a launcher class for the producer tasks.
+//
+class ProducerTasks: public IndexLauncher {
 public:
-  PCTask(const Domain &launch_domain,
-	 const ArgumentMap &arg_map);
+  ProducerTasks(const Domain &launch_domain, const ArgumentMap &arg_map);
 public:
+  static const int TASK_ID = PRODUCER_ID;  // The id of the tasks that will be launched.
+  static const int MAPPER_ID = 0;
+};
+
+ProducerTasks::ProducerTasks(const Domain &launch_domain,const ArgumentMap &arg_map) : 
+  IndexLauncher(PRODUCER_ID,          // the ID of the tasks this index launcher will execute
+		launch_domain,        // identities of the tasks to launch (an index space)
+                TaskArgument(NULL,0), // arguments passed to all the tasks, not used in this example
+                arg_map,              // a map from the launch domain to the argument for each individual task
+                Predicate::TRUE_PRED, // these tasks can be indexed launched
+                false,                // these tasks cannot be single launched
+                0)                    // an id for the mapper --- not used in this example
+{
+
+}
+
+class ConsumerTasks: public IndexLauncher {
+public:
+  ConsumerTasks(const Domain &launch_domain,
+	       const ArgumentMap &arg_map);
+public:
+  static const int TASK_ID = CONSUMER_ID;
+  static const int MAPPER_ID = 0;
+};
+
+ConsumerTasks::ConsumerTasks(const Domain &launch_domain,const ArgumentMap &arg_map) : 
+   IndexLauncher(CONSUMER_ID, launch_domain, TaskArgument(NULL,0), arg_map, Predicate::TRUE_PRED, false, 0)
+{
+
+}
+
 void top_level_task(const Task *task,
 		    const std::vector<PhysicalRegion> &regions,
 		    Context ctx, 
 		    Runtime *runtime)
+
 {
+  // Launch 50 tasks.
   int points = 50;
   const Rect<1> launch_domain(1,points);
   ArgumentMap producer_arg_map;
   for (int i = 0; i < points; i += 1)
   {
     int subtask_id = 2*i;
-    producer_arg_map.set_point(DomainPoint::from_point<1>(i+1),
-			       TaskArgument(&subtask_id,sizeof(int)));
+    producer_arg_map.set_point(DomainPoint::from_point<1>(i+1), TaskArgument(&subtask_id,sizeof(int)));
   }
-  IndexLauncher producer_launcher(INDEX_PRODUCER_ID,
-				  launch_domain,
-				  TaskArgument(NULL, 0),
-				  producer_arg_map);
+  ProducerTasks  producer_launcher(launch_domain, producer_arg_map);
+  // 
+  //  Since each producer task returns an integer, the index launch will return a FutureMap, a map from
+  //  the launch domain to a future for each point.  The FutureMap can be used as an ArgumentMap to a subsequent
+  //  index launch.
+  //
   FutureMap fm = runtime->execute_index_space(ctx, producer_launcher);
   ArgumentMap consumer_arg_map(fm);
-  IndexLauncher consumer_launcher(INDEX_CONSUMER_ID,
-				  launch_domain,
-				  TaskArgument(NULL, 0),
-				  consumer_arg_map);
+  ConsumerTasks consumer_launcher(launch_domain, consumer_arg_map);
   runtime->execute_index_space(ctx, consumer_launcher);
 }
 
@@ -72,12 +107,12 @@ int main(int argc, char **argv)
     Runtime::preregister_task_variant<top_level_task>(registrar);
   }
   {
-    TaskVariantRegistrar registrar(INDEX_PRODUCER_ID, "index_producer");
+    TaskVariantRegistrar registrar(PRODUCER_ID, "index_producer");
     registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
     Runtime::preregister_task_variant<int,subtask_producer>(registrar);
   }
   {
-    TaskVariantRegistrar registrar(INDEX_CONSUMER_ID, "index_consumer");
+    TaskVariantRegistrar registrar(CONSUMER_ID, "index_consumer");
     registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
     Runtime::preregister_task_variant<subtask_consumer>(registrar);
   }
