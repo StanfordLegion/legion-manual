4d2ae35d835bde7ad9277d6e3f2d149d2c469dde
Alex Aiken <aiken@Alexs-iMac.local>
2022-08-25 20:59:31 -0700

    atomicity example

---
+++ Examples/Coherence/atomic/atomic.cc
@@ -0,0 +1,101 @@
+#include <cstdio>
+#include "legion.h"
+
+using namespace Legion;
+
+enum TaskIDs {
+  TOP_LEVEL_TASK_ID,
+  SUM_TASK_ID,
+  INC_TASK_ID,
+};
+
+enum FieldIDs {
+  FIELD_A,
+};
+
+void top_level_task(const Task *task,
+		    const std::vector<PhysicalRegion> &rgns,
+		    Context ctx, 
+		    Runtime *rt)
+{
+  Rect<1> rec(Point<1>(0),Point<1>(99));
+  IndexSpace is = rt->create_index_space(ctx,rec);
+  FieldSpace fs = rt->create_field_space(ctx);
+  FieldAllocator field_allocator = rt->create_field_allocator(ctx,fs);
+  FieldID fida = field_allocator.allocate_field(sizeof(int), FIELD_A);
+  assert(fida == FIELD_A);
+
+
+  LogicalRegion lr = rt->create_logical_region(ctx,is,fs);
+  
+  int init = 1;
+  rt->fill_field(ctx,lr,lr,fida,&init,sizeof(init));
+
+  for (int i = 0; i < 10; i++) {
+    TaskLauncher inc_launcher(INC_TASK_ID, TaskArgument(&i,sizeof(int)));
+    inc_launcher.add_region_requirement(RegionRequirement(lr, READ_WRITE, ATOMIC, lr));
+    inc_launcher.add_field(0,FIELD_A);
+    rt->execute_task(ctx, inc_launcher);
+  }
+  
+  TaskLauncher sum_launcher(SUM_TASK_ID, TaskArgument(NULL,0));
+  sum_launcher.add_region_requirement(RegionRequirement(lr, READ_ONLY, EXCLUSIVE, lr));
+  sum_launcher.add_field(0,FIELD_A);
+  rt->execute_task(ctx, sum_launcher);
+
+  // Clean up.  IndexAllocators and FieldAllocators automatically have their resources reclaimed
+  // when they go out of scope.
+  rt->destroy_logical_region(ctx,lr);
+  rt->destroy_field_space(ctx,fs);
+  rt->destroy_index_space(ctx,is);
+}
+
+void inc_task(const Task *task,
+		    const std::vector<PhysicalRegion> &rgns,
+		    Context ctx, Runtime *rt)
+{
+  int id = *((int *) task->args);
+  const FieldAccessor<READ_WRITE,int,1> fa_a(rgns[0], FIELD_A);
+  Rect<1> d = rt->get_index_space_domain(ctx,task->regions[0].region.get_index_space());
+  printf("Task %d\n",id);
+  for (PointInRectIterator<1> itr(d); itr(); itr++)
+    {
+      fa_a[*itr] = fa_a[*itr] + 1;
+    }
+}
+
+void sum_task(const Task *task,
+		    const std::vector<PhysicalRegion> &rgns,
+		    Context ctx, Runtime *rt)
+{
+  const FieldAccessor<READ_ONLY,int,1> fa_a(rgns[0], FIELD_A);
+  Rect<1> d = rt->get_index_space_domain(ctx,task->regions[0].region.get_index_space());
+  int sum = 0;
+  for (PointInRectIterator<1> itr(d); itr(); itr++)
+    {
+      sum += fa_a[*itr]; 
+    }
+  printf("The sum of the elements of the region is %d\n",sum);
+}
+
+int main(int argc, char **argv)
+{
+  Runtime::set_top_level_task_id(TOP_LEVEL_TASK_ID);
+  {
+    TaskVariantRegistrar registrar(TOP_LEVEL_TASK_ID, "top_level_task");
+    registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
+    Runtime::preregister_task_variant<top_level_task>(registrar);
+  }
+  {
+    TaskVariantRegistrar registrar(INC_TASK_ID, "inc_task");
+    registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
+    Runtime::preregister_task_variant<inc_task>(registrar);
+  }
+  {
+    TaskVariantRegistrar registrar(SUM_TASK_ID, "sum_task");
+    registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
+    Runtime::preregister_task_variant<sum_task>(registrar);
+  }
+  return Runtime::start(argc, argv);
+}
+
